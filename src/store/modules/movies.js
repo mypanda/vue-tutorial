be72bf003f5f30ab64822cd55ffa48bf996acb49
mypanda <951788234@qq.com>
2017-06-09 16:40:15 +0800

    tmp movies.js

---
+++ src/store/modules/movies.js
@@ -0,0 +1,168 @@
+/**
+ 
+api.js
+const _products = [
+  {"id": 1, "title": "iPad 4 Mini", "price": 500.01, "inventory": 2},
+  {"id": 2, "title": "H&M T-Shirt White", "price": 10.99, "inventory": 10},
+  {"id": 3, "title": "Charli XCX - Sucker CD", "price": 19.99, "inventory": 5}
+]
+
+export default {
+  getProducts (cb) {
+    setTimeout(() => cb(_products), 100)
+  },
+
+  buyProducts (products, cb, errorCb) {
+    setTimeout(() => {
+      // simulate random checkout failure.
+      (Math.random() > 0.5 || navigator.userAgent.indexOf('PhantomJS') > -1)
+        ? cb()
+        : errorCb()
+    }, 100)
+  }
+}
+
+
+
+ */
+
+/*
+import shop from '../../api/shop'
+import * as types from '../mutation-types'
+
+// initial state
+// shape: [{ id, quantity }]
+const state = {
+  added: [],
+  checkoutStatus: null
+}
+
+// getters
+const getters = {
+  checkoutStatus: state => state.checkoutStatus
+}
+
+// actions
+const actions = {
+  checkout ({ commit, state }, products) {
+    const savedCartItems = [...state.added]
+    commit(types.CHECKOUT_REQUEST)
+    shop.buyProducts(
+      products,
+      () => commit(types.CHECKOUT_SUCCESS),
+      () => commit(types.CHECKOUT_FAILURE, { savedCartItems })
+    )
+  }
+}
+
+// mutations
+const mutations = {
+  [types.ADD_TO_CART] (state, { id }) {
+    state.lastCheckout = null
+    const record = state.added.find(p => p.id === id)
+    if (!record) {
+      state.added.push({
+        id,
+        quantity: 1
+      })
+    } else {
+      record.quantity++
+    }
+  },
+
+  [types.CHECKOUT_REQUEST] (state) {
+    // clear cart
+    state.added = []
+    state.checkoutStatus = null
+  },
+
+  [types.CHECKOUT_SUCCESS] (state) {
+    state.checkoutStatus = 'successful'
+  },
+
+  [types.CHECKOUT_FAILURE] (state, { savedCartItems }) {
+    // rollback to the cart saved before sending the request
+    state.added = savedCartItems
+    state.checkoutStatus = 'failed'
+  }
+}
+
+export default {
+  state,
+  getters,
+  actions,
+  mutations
+}
+
+*/
+
+
+//导入api
+// import shop from '../../api/shop'
+//导入函数名字
+import * as types from '../mutation-types'
+
+// initial state 初始化数据
+// shape: [{ id, quantity }]
+const state = {
+  added: [],
+  checkoutStatus: null
+}
+
+// getters 处理二次数据
+const getters = {
+  checkoutStatus: state => state.checkoutStatus
+}
+
+// actions 处理异步
+const actions = {
+  checkout ({ commit, state }, products) {
+    const savedCartItems = [...state.added]
+    commit(types.CHECKOUT_REQUEST)
+    shop.buyProducts(
+      products,
+      () => commit(types.CHECKOUT_SUCCESS),
+      () => commit(types.CHECKOUT_FAILURE, { savedCartItems })
+    )
+  }
+}
+
+// mutations 操作数据函数，不异步
+const mutations = {
+  [types.ADD_TO_CART] (state, { id }) {
+    state.lastCheckout = null
+    const record = state.added.find(p => p.id === id)
+    if (!record) {
+      state.added.push({
+        id,
+        quantity: 1
+      })
+    } else {
+      record.quantity++
+    }
+  },
+
+  [types.CHECKOUT_REQUEST] (state) {
+    // clear cart
+    state.added = []
+    state.checkoutStatus = null
+  },
+
+  [types.CHECKOUT_SUCCESS] (state) {
+    state.checkoutStatus = 'successful'
+  },
+
+  [types.CHECKOUT_FAILURE] (state, { savedCartItems }) {
+    // rollback to the cart saved before sending the request
+    state.added = savedCartItems
+    state.checkoutStatus = 'failed'
+  }
+}
+
+//默认导出四个
+export default {
+  state,
+  getters,
+  actions,
+  mutations
+}
